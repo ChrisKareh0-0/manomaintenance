78d2f8d16acb81f0ba85d1602c38637c04630b6b
Chris <chriskareh@proton.me>
2025-04-18 18:51:19 +0300

    fix navigation

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,12 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
 
 export const Navigation = (props) => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleNav = () => {
-    setIsOpen(!isOpen);
-  };
-
   return (
     <nav id="menu" className="navbar navbar-default navbar-fixed-top">
       <div className="container">
@@ -16,16 +10,59 @@ export const Navigation = (props) => {
             className="navbar-toggle collapsed"
             data-toggle="collapse"
             data-target="#bs-example-navbar-collapse-1"
-            onClick={toggleNav}
           >
-            <span className="sr-only">Toggle navigation</span>
-            <span className="icon-bar"></span>
-            <span className="icon-bar"></span>
-            <span className="icon-bar"></span>
+            {" "}
+            <span className="sr-only">Toggle navigation</span>{" "}
+            <span className="icon-bar"></span>{" "}
+            <span className="icon-bar"></span>{" "}
+            <span className="icon-bar"></span>{" "}
           </button>
           <a className="navbar-brand page-scroll" href="#page-top">
             ManoMaintenance
-          </a>
+          </a>{" "}
+        </div>
+
+        <div
+          className="collapse navbar-collapse"
+          id="bs-example-navbar-collapse-1"
+        >
+          <ul className="nav navbar-nav navbar-right">
+            {/* <li>
+              <a href="#features" className="page-scroll">
+                Features
+              </a>
+            </li> */}
+            <li>
+              <a href="#about" className="page-scroll">
+                About
+              </a>
+            </li>
+            <li>
+              <a href="#services" className="page-scroll">
+                Services
+              </a>
+            </li>
+            <li>
+              <a href="#portfolio" className="page-scroll">
+                Gallery
+              </a>
+            </li>
+            {/* <li>
+              <a href="#testimonials" className="page-scroll">
+                Testimonials
+              </a>
+            </li> */}
+            {/* <li>
+              <a href="#team" className="page-scroll">
+                Team
+              </a>
+            </li> */}
+            {/* <li>
+              <a href="#contact" className="page-scroll">
+                Contact
+              </a>
+            </li> */}
+          </ul>
         </div>
       </div>
     </nav>
